09cb760a2898ae49da318253eaca6eebd6dc39ea
Aditya Ambadipudi <ganeshaditya1@gmail.com>
2025-05-17 02:13:50 +0530

    Add all the MDX parsing libraries
    Various MDX parsing libraries are used in this blog
    rehypeKatex for parsing latex code
    rehypeStarryNight for pretty print code snippets
    
    remarkGfm for parsing all the md syntax that github supports(Github md
    has a lot more bells and whistles than ordinary md)
    remarkFrontmatter, remarkMdxFrontMatter for parsing the metadata header at the top of the mdx
    files in the posts.
    remarkMath works in tandem with remarkatex
    remarkToc for generating table of content.

---
+++ next.config.ts
@@ -0,0 +1,36 @@
+import type { NextConfig } from "next";
+
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
+
+import rehypeKatex from 'rehype-katex'
+import remarkMath from 'remark-math'
+
+import rehypeStarryNight from 'rehype-starry-night'
+
+import remarkToc from 'remark-toc'
+
+import remarkGfm from 'remark-gfm'
+import createMDX from '@next/mdx';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  experimental: {
+    mdxRs: false,
+  }
+};
+
+
+
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    rehypePlugins: [rehypeKatex, [rehypeStarryNight, ]],
+    remarkPlugins: [remarkGfm, remarkFrontmatter, remarkMdxFrontmatter, remarkMath, remarkToc],
+  },
+})
+
+export default withMDX(nextConfig);
