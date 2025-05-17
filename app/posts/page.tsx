dd59a493e48e0bf25c5b6ac9fcd00eeda3b61df4
Aditya Ambadipudi <ganeshaditya1@gmail.com>
2025-05-17 21:10:11 +0530

    Add metadata to all posts page.
    Add title and description metadata.

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,6 +1,11 @@
-
+import type { Metadata } from "next";
 import Posts from '@/components/posts';
 import { getSlugsFromDirectory, getPostFromMdxFile } from '@/app/mdxUtil';
+
+export const metadata: Metadata = {
+  title: "About me",
+  description: "Aditya Ambadipudi about me page, detailing my career journey so far.",
+};
 
 export default async function Page({}) {
     const slugs = (await getSlugsFromDirectory('posts'))
