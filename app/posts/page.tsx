5d64c5fc6a7b040031fb80cf5892b579462c528e
Aditya Ambadipudi <ganeshaditya1@gmail.com>
2025-05-17 21:06:49 +0530

    Move page that shows all posts into pages subdir
    Currently the page that shows all posts is in the top level app
    directory. At this level we should show the about me page instead.
    Hence copying this into the right directory.

---
+++ app/posts/page.tsx
@@ -0,0 +1,14 @@
+
+import Posts from '@/components/posts';
+import { getSlugsFromDirectory, getPostFromMdxFile } from '@/app/mdxUtil';
+
+export default async function Page({}) {
+    const slugs = (await getSlugsFromDirectory('posts'))
+      .map((slug) => slug.slug);
+
+    const postPromises = slugs.map((slug) => getPostFromMdxFile(slug));
+    return Promise.all(postPromises)
+                  .then((posts) => (< Posts posts={ posts } />));
+  }
+   
+  export const dynamicParams = false
