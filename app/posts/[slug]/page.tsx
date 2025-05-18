d370f2b06f3abb7639d5da0b7adc29328bde93b9
Aditya Ambadipudi <ganeshaditya1@gmail.com>
2025-05-18 18:53:52 +0530

    Set appropriate dynamic metadata for post pages.
    Set title, description and tags of metadata of each post page.

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,5 +1,22 @@
 import Post from '@/components/post';
+import type { Metadata } from 'next';
 import { getSlugsFromDirectory, getPostFromMdxFile } from '@/app/mdxUtil';
+
+export async function generateMetadata({ params }: {
+    params: Promise<{ slug: string }>
+  }): Promise<Metadata> {
+  const slug = (await params).slug
+  
+  // Fetch the post for the given slug.
+  const post = await getPostFromMdxFile(slug);
+ 
+  return {
+    title: post.title,
+    description: post.synopsis,
+    keywords: post.tags
+  }
+}
+ 
 
 export default async function Page({ params }: {
     params: Promise<{ slug: string }>
