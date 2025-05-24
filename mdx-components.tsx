7f0e8dc4596c25456c0dcfe61fe04a7ff1366691
Aditya Ambadipudi <ganeshaditya1@gmail.com>
2025-05-24 23:31:29 +0530

    Add a border around the code block generated by starry-night plugin.

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,6 +2,14 @@ import type { MDXComponents } from 'mdx/types'
  
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    // Custom component for code blocks
+    pre: ({ children }) => (
+      <div className="bg-gray-100 rounded-md border border-gray-200">
+      <pre style={{ padding: '10px' }}>
+        <code>{children}</code>
+      </pre>
+      </div>
+    ),
     ...components,
   }
 }
