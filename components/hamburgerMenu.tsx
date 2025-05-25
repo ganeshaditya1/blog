78640b3cef206ef05e5862be82568d7ce65bed4d
Aditya Ambadipudi <ganeshaditya1@gmail.com>
2025-05-25 15:39:12 +0530

    Change mouse cursor for all textbased links
    The hamburger menu and in chess puzzles all the buttons for a puzzle
    like the hint buttons and the solution button are all textbased buttons.
    When the user hovers their mouse over them, appropriate cursor needs to
    be used to indicate to the user that the text is clickable.

--- components/hamburgerMenu.tsx
+++ components/hamburgerMenu.tsx
@@ -32,7 +32,7 @@ export default function HamburgerMenu() {
 
     return (
         <div className="block xl:hidden">
-            <p onClick={onclickHandler}>
+            <p onClick={onclickHandler} style={{cursor: 'pointer'}}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="inline size-6" style={{ verticalAlign: "top" }}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg> &nbsp;
